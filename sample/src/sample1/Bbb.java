c26d1ff9dca761e6668aece0e36ae93042e6b900
mte <hnnrplayer@gmail.com>
2017-03-04 22:21:30 +0900

    world to work

--- sample/src/sample1/Bbb.java
+++ sample/src/sample1/Bbb.java
@@ -4,7 +4,7 @@ public class Bbb {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("heloo wordl");
+			System.out.println("heloo wwork");
 	}
 
 }
